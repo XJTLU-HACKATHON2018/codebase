f386e8f6345657c03a3a6912ef3f7a9d87c3d073
horace <billweasley20092@gmail.com>
2018-06-24 01:05:36 +0800

    attempt wrapping backtest network (failed)

--- mercurius/web/app.py
+++ mercurius/web/app.py
@@ -2,12 +2,16 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 
+import datetime as dt
+
 from flask import Flask, request, json
 from mercurius.data import candlereader
+from backtest import backtest
 import numpy as np
 import ccxt
 
 app = Flask(__name__)
+
 
 @app.route('/uploadAlgo', methods=['POST', 'GET'])
 def run_algo():
@@ -20,46 +24,66 @@ def run_algo():
         symbols = data["symbols"]
         da = candlereader.CandleReader(symbols, start_time, end_time, timeframe, exchange).get_data()
         close_price = da.sel(feature='close')
-        #algo.run()
+        # algo.run()
         response = {}
         response['close'] = close_price.data.tolist()
-        response['date'] = [x/1e6 for x in da['openTime'].values.astype(np.int64)]
-        #print(response['date'])
-        #print(type(response['date']))
+        response['date'] = [x / 1e6 for x in da['openTime'].values.astype(np.int64)]
+        # print(response['date'])
+        # print(type(response['date']))
         print('PASS')
         return json.jsonify(response)
     print('FAIL')
     return 'FAIL'
 
+
 @app.route('/coin/<symbol>', methods=['POST', 'GET'])
 def get_coin_price(coin):
     """default poloniex"""
     if request.method == "POST":
-        symbol = str(coin).upper()+"/BTC"
+        symbol = str(coin).upper() + "/BTC"
         df = candlereader.CandleReader(symbol).get_data().to_pandas()
         print(df)
         print(type(df))
         return 'PASS'
     return 'FAIL'
 
+
 @app.route('/balances', methods=['GET'])
 def get_balance(exchange="binance"):
     assets = ['BTC', 'VEN', 'BNB', 'USDT']
-    exchange = getattr(ccxt, exchange)({'apiKey': '8f8tN9PmXCQSfU4RpBvE0Y8vQioQkbH2vUkVC6cS0jTpSplGufBxSmAOpkQokYt4', 'secret': '9w0aoElX1iRrhOOptnMiYRei57sBAZQmMZKQpwsp3IeYqBTP7LpsRJhhFvO1WWFv'})
+    exchange = getattr(ccxt, exchange)({'apiKey': '8f8tN9PmXCQSfU4RpBvE0Y8vQioQkbH2vUkVC6cS0jTpSplGufBxSmAOpkQokYt4',
+                                        'secret': '9w0aoElX1iRrhOOptnMiYRei57sBAZQmMZKQpwsp3IeYqBTP7LpsRJhhFvO1WWFv'})
     balances = exchange.fetchBalance()
     res = {}
     for i in assets:
         res[i] = balances[i]
     return json.jsonify(res)
 
+
 @app.route('/orders', methods=['GET'])
 def get_orders(exchange="binance"):
-    pairs = ['ETH/BTC', 'VEN/BTC', 'BNB/BTC', 'BTC/USDT'] #we could include this as input parameter later
-    exchange = getattr(ccxt, exchange)({'apiKey': '8f8tN9PmXCQSfU4RpBvE0Y8vQioQkbH2vUkVC6cS0jTpSplGufBxSmAOpkQokYt4', 'secret': '9w0aoElX1iRrhOOptnMiYRei57sBAZQmMZKQpwsp3IeYqBTP7LpsRJhhFvO1WWFv'})
+    pairs = ['ETH/BTC', 'VEN/BTC', 'BNB/BTC', 'BTC/USDT']  # we could include this as input parameter later
+    exchange = getattr(ccxt, exchange)({'apiKey': '8f8tN9PmXCQSfU4RpBvE0Y8vQioQkbH2vUkVC6cS0jTpSplGufBxSmAOpkQokYt4',
+                                        'secret': '9w0aoElX1iRrhOOptnMiYRei57sBAZQmMZKQpwsp3IeYqBTP7LpsRJhhFvO1WWFv'})
     res = {}
     for i in pairs:
         res[i] = exchange.fetchOrders(i)
     return json.jsonify(res)
+
+
+### Illness of network
+@app.route('/backtest/<starttimestamp>/<endtimestamp>', methods=['GET'])
+
+def get_backtest(starttimestamp, endtimestamp):
+    res = backtest(
+        dt.datetime.fromtimestamp(int(starttimestamp)).strftime("%Y-%m-%d %H:%M:%S"),
+        dt.datetime.fromtimestamp(int(endtimestamp)).strftime("%Y-%m-%d %H:%M:%S"),
+        location="../../train_package/"
+    )
+    res = backtest("2018-06-02 00:00:00", "2018-06-06 00:00:00", location="../../train_package/")
+
+    values = res.get("portfolio_changes_history").tolist()
+    return json.jsonify(values)
 
 
 def main():
