93365fd318b2bf094395ad83ee38d76707578159
GerardLiu96 <452005231@qq.com>
2018-06-23 19:59:10 +0800

    multithreading SentimentProcess

--- SentimentProcess/SentimentProcess.py
+++ SentimentProcess/SentimentProcess.py
@@ -5,16 +5,27 @@ import re
 import pandas as pd
 import TimeT
 import json
+import threading
 from langdetect import detect
 
 def printTweet(descr, t):
-	print(descr)
-	print("Username: %s" % t.username)
-	print("Retweets: %d" % t.retweets)
-	print("Text: %s" % t.text)
-	print("Mentions: %s" % t.mentions)
-	print("Time: %s\n" % str(t.date))
+    print(descr)
+    print("Username: %s" % t.username)
+    print("Retweets: %d" % t.retweets)
+    print("Text: %s" % t.text)
+    print("Mentions: %s" % t.mentions)
+    print("Time: %s\n" % str(t.date))
 
+class myThread (threading.Thread):
+    def __init__(self,  name, start_date, end_date):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.start_date = start_date
+        self.end_date = end_date
+
+    def run(self):
+        print("Starting Thread："+self.name)
+        save_data(start_date=self.start_date, end_date=self.end_date)
 
 def clean_tweet(tweet):
 	'''
@@ -68,10 +79,11 @@ def get_twitter_sentiment(assets, start_date, end_date):
 	for asset in assets:
 		date_sentiment_list = []
 		date_raw_data = []
-		file = open('raw.json', "a+")
 		for date in dates:
-			tweets = get_tweets(query=asset, count=100, since='2014-01-01', until=date)
+			tweets = get_tweets(query=asset, count=100, since='2018-01-01', until=date)
+			file = open('raw.json', "a+")
 			json.dump(tweets, file)
+			file.close()
 			sums = 0
 			for tweetp in tweets:
 				sums = sums + tweetp['sentiment']
@@ -80,26 +92,39 @@ def get_twitter_sentiment(assets, start_date, end_date):
 			date_raw_data.append(tweets)
 		column[asset] = date_sentiment_list
 		rawdata[asset] = date_raw_data
-		file.close()
 
 	return pd.DataFrame(column, index=dates), rawdata
 
-def main():
-	'''
-	tweetCriteria = got.manager.TweetCriteria().setQuerySearch('ethereum').setSince("2018-06-01").setUntil("2018-06-02").setMaxTweets(100)
-	tweets = got.manager.TweetManager.getTweets(tweetCriteria)
-	print(len(tweets))
-
-	for tweet in tweets:
-		printTweet("### Example 2 - Get tweets by query search [europe refugees]", tweet)
-	'''
+def save_data(start_date, end_date):
 	assets = ['$ETH', '$LTC', '$XRP', '$ETC', '$DASH', '$XMR', '$XEM', '$FCT', '$GNT', '$ZEC', '$BTC']
 	save1, save2 = get_twitter_sentiment(assets=assets, start_date=sys.argv[1], end_date=sys.argv[2])
-	save1.to_csv(sys.argv[1]+"--"+sys.argv[2]+'.csv', index=True, sep=',')
+	save1.to_csv(sys.argv[1] + "--" + sys.argv[2] + '.csv', index=True, sep=',')
 
-	file = open(sys.argv[1]+"--"+sys.argv[2]+'-raw.json', "w+")
+	file = open(sys.argv[1] + "--" + sys.argv[2] + '-raw.json', "w+")
 	json.dump(save2, file)
 	file.close()
+
+def main():
+    start_date = sys.argv[1]
+    end_date = sys.argv[2]
+    startTS = TimeT.trans_to_timestamp(start_date)
+    endTS = TimeT.trans_to_timestamp(end_date)
+    period = int((endTS-startTS)/8)
+    n = 0
+    threads = []
+    while (startTS <= endTS):
+        end = startTS + period
+        thread = myThread(str(n), start_date=startTS, end_date=end)
+        threads.append(thread)
+        startTS = startTS + period
+        n = n+1
+
+    for thread in threads:
+        thread.start()
+
+    for t in threads:
+        t.join()
+
 
 if __name__ == '__main__':
 	main()
