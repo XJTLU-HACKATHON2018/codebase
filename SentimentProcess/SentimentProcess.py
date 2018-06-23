0fdb41e84c46fef7513e1a30a7c949200f3e123d
GerardLiu96 <452005231@qq.com>
2018-06-23 18:15:58 +0800

    SentimentProcess v1.0

--- SentimentProcess/SentimentProcess.py
+++ SentimentProcess/SentimentProcess.py
@@ -1,124 +1,105 @@
-import re
-import tweepy
-import TimeT
-from tweepy import OAuthHandler
+import sys
+import got3 as got
 from textblob import TextBlob
+import re
 import pandas as pd
+import TimeT
+import json
+from langdetect import detect
+
+def printTweet(descr, t):
+	print(descr)
+	print("Username: %s" % t.username)
+	print("Retweets: %d" % t.retweets)
+	print("Text: %s" % t.text)
+	print("Mentions: %s" % t.mentions)
+	print("Time: %s\n" % str(t.date))
 
 
-class TwitterClient(object):
+def clean_tweet(tweet):
+	'''
+    Utility function to clean tweet text by removing links, special characters
+    using simple regex statements.
     '''
-    Generic Twitter Class for sentiment analysis.
+	return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])| (\w+:\ / \ / \S+)", " ", tweet).split())
+
+def get_tweet_sentiment(tweet):
+	# create TextBlob object of passed tweet text
+	analysis = TextBlob(clean_tweet(tweet))
+	# set sentiment
+	return analysis.sentiment.polarity
+
+
+def get_tweets(query, count, since, until):
+	'''
+    Main function to fetch tweets and parse them.
     '''
-
-    def __init__(self):
-        '''
-        Class constructor or initialization method.
-        '''
-        # keys and tokens from the Twitter Dev Console
-        consumer_key = 'eFYfSvT05fejti8wBQDXELjMH'
-        consumer_secret = 'dO5q1CVGvnGjvbsZ2rGEfEici2zj3P8vtqJsL2B2kguQGviOZZ'
-        access_token = '1010079466511478784-BKibdGczsxMJ73xJAHfkAugNiNhjaT'
-        access_token_secret = 'W8euzQSLYZfFElt1CRqNMYxXpD4Un39MlKIAtIwMdN21O'
-
-        # attempt authentication
-        try:
-            # create OAuthHandler object
-            self.auth = OAuthHandler(consumer_key, consumer_secret)
-            # set access token and secret
-            self.auth.set_access_token(access_token, access_token_secret)
-            # create tweepy API object to fetch tweets
-            self.api = tweepy.API(self.auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
-        except:
-            print("Error: Authentication Failed")
-
-    def clean_tweet(self, tweet):
-        '''
-        Utility function to clean tweet text by removing links, special characters
-        using simple regex statements.
-        '''
-        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])| (\w+:\ / \ / \S+)", " ", tweet).split())
-
-    def get_tweet_sentiment(self, tweet):
-        '''
-        Utility function to classify sentiment of passed tweet
-        using textblob's sentiment method
-        '''
-        # create TextBlob object of passed tweet text
-        analysis = TextBlob(self.clean_tweet(tweet))
-        # set sentiment
-        return analysis.sentiment.polarity
-
-    def get_tweets(self, query, count, until):
-        '''
-        Main function to fetch tweets and parse them.
-        '''
-        # empty list to store parsed tweets
-        tweets = []
-        try:
-            # parsing tweets one by one
-            for tweet in tweepy.Cursor(self.api.search, q=query, until=until, lang="en").items(count):
-                # empty dictionary to store required params of a tweet
-                parsed_tweet = {}
-
-                # saving text of tweet
-                parsed_tweet['text'] = tweet.text
-                # saving sentiment of tweet
-                # parsed_tweet['time'] = tweet.created_at
-                parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text)
-
-                # appending parsed tweet to tweets list
-                if tweet.retweet_count > 0:
-                    # if tweet has retweets, ensure that it is appended only once
-                    if parsed_tweet not in tweets:
-                        tweets.append(parsed_tweet)
-                else:
-                    tweets.append(parsed_tweet)
-
-            # return parsed tweets
-            return tweets
-
-        except tweepy.TweepError as e:
-            # print error (if any)
-            print("Error : " + str(e))
-
+	# empty list to store parsed tweets
+	tweets = []
+	while(len(tweets)<count):
+		tweetCriteria = got.manager.TweetCriteria().setQuerySearch(query).setSince(since).setUntil(until).setMaxTweets(count)
+		tweetss = got.manager.TweetManager.getTweets(tweetCriteria)
+		for tweet in tweetss:
+			if(detect(tweet.text) == 'en'):
+				parsed_tweet = {}
+				parsed_tweet['text'] = tweet.text
+				parsed_tweet['time'] = tweet.date.strftime("%Y-%m-%d")
+				parsed_tweet['sentiment'] = get_tweet_sentiment(tweet.text)
+				if tweet.retweets > 0:
+					if parsed_tweet not in tweets:
+						tweets.append(parsed_tweet)
+				else:
+					tweets.append(parsed_tweet)
+			else: pass
+	print(str(len(tweets))+"Successful")
+	return tweets
 
 def get_twitter_sentiment(assets, start_date, end_date):
+	startTS = TimeT.trans_to_timestamp(start_date)
+	endTS = TimeT.trans_to_timestamp(end_date)
+	dates = []
+	while (startTS <= endTS):
+		dates.append(TimeT.trans_to_Date(startTS))
+		startTS = startTS + 86400
 
-    api = TwitterClient()
+	column = {}
+	rawdata = {}
 
-    startTS = TimeT.trans_to_timestamp(start_date)
-    endTS = TimeT.trans_to_timestamp(end_date)
-    dates = []
-    while(startTS<=endTS):
-        dates.append(TimeT.trans_to_Date(startTS))
-        startTS = startTS+86400
+	for asset in assets:
+		date_sentiment_list = []
+		date_raw_data = []
+		file = open('raw.json', "a+")
+		for date in dates:
+			tweets = get_tweets(query=asset, count=100, since='2014-01-01', until=date)
+			json.dump(tweets, file)
+			sums = 0
+			for tweetp in tweets:
+				sums = sums + tweetp['sentiment']
+			n = sums / len(tweets)
+			date_sentiment_list.append(n)
+			date_raw_data.append(tweets)
+		column[asset] = date_sentiment_list
+		rawdata[asset] = date_raw_data
+		file.close()
 
-    column = {}
-
-    for asset in assets:
-        date_sentiment_list = []
-        for date in dates:
-            tweets = api.get_tweets(query=asset, count=500, until=date)
-            sums = 0
-            for tweetp in tweets:
-                sums = sums + tweetp['sentiment']
-            n = sums / len(tweets)
-            date_sentiment_list.append(n)
-        column[asset] = date_sentiment_list
-
-    return pd.DataFrame(column, index=dates)
-
+	return pd.DataFrame(column, index=dates), rawdata
 
 def main():
+	'''
+	tweetCriteria = got.manager.TweetCriteria().setQuerySearch('ethereum').setSince("2018-06-01").setUntil("2018-06-02").setMaxTweets(100)
+	tweets = got.manager.TweetManager.getTweets(tweetCriteria)
+	print(len(tweets))
 
-    # assets = ['Litecoin', 'EOSIO', 'Ethereum', 'Bitcoin']
-    assets = ['$LTC', '$EOS', '$ETH', '$BTC']
-    save = get_twitter_sentiment(assets=assets, start_date='2018-06-11', end_date='2018-06-21')
-    save.to_csv('test.csv', index=True, sep=',')
+	for tweet in tweets:
+		printTweet("### Example 2 - Get tweets by query search [europe refugees]", tweet)
+	'''
+	assets = ['$ETH', '$LTC', '$XRP', '$ETC', '$DASH', '$XMR', '$XEM', '$FCT', '$GNT', '$ZEC', '$BTC']
+	save1, save2 = get_twitter_sentiment(assets=assets, start_date=sys.argv[1], end_date=sys.argv[2])
+	save1.to_csv(sys.argv[1]+"--"+sys.argv[2]+'.csv', index=True, sep=',')
 
+	file = open(sys.argv[1]+"--"+sys.argv[2]+'-raw.json', "w+")
+	json.dump(save2, file)
+	file.close()
 
-
-if __name__ == "__main__":
-    # calling main function
-    main()
+if __name__ == '__main__':
+	main()
